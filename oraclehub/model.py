a99f6f0cb8efc25356d71328515b4f0e0527ea8a
Hessam Mehr <Hessam.Mehr@glasgow.ac.uk>
2022-08-25 16:35:22 +0000

    WIP: Implement model hosting.

--- oraclehub/model.py
+++ oraclehub/model.py
@@ -1,15 +1,25 @@
 from __future__ import annotations
+from curses import meta
+from email.policy import default
+from importlib.metadata import metadata
 
 import pandas as pd
 from numpyro import distributions as dist
 
 
 class Model:
-    def __init__(self):
-        pass
+    metadata = {}
+    def __init__(self, metadata: dict):
+        for metaname, val in self.metadata.items():
+            if isinstance(val, str):
+                # no default
+                metavalue = metadata[metaname]
+            else:
+                metavalue = metadata.get(metaname, val[0])
+            setattr(self, metaname, metavalue)
 
     def predict(self, experiments) -> dict[str, dist.Distribution]:
         pass
 
-    def post_process(self, data: pd.DataFrame) -> pd.DataFrame:
+    def post_process(self, data: pd.DataFrame) -> dict[str, np.ndarray]:
         pass
--- oraclehub/oraclehub.py
+++ oraclehub/oraclehub.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
+from argparse import Namespace
 
 import inspect
 from hashlib import sha256
 from pathlib import Path
 import pickle
 
+from contextlib import contextmanager
+from typing import final
 import pandas as pd
 from jax import numpy as jnp
 from jax.random import PRNGKey
@@ -15,12 +18,11 @@ from numpyro.infer import MCMC, NUTS
 
 from oraclehub.model import Model
 
-# %%%
-
 
 class OracleHub:
     def __init__(self, db_file=None):
         self.db = {}
+        self._meta = None
         if db_file:
             self.db_file = Path(db_file)
         else:
@@ -28,6 +30,14 @@ class OracleHub:
 
         if self.db_file.exists():
             self.load()
+
+    @contextmanager
+    def with_meta(self, meta: dict):
+        try:
+            self._meta = meta
+            yield self
+        finally:
+            self._meta = None
 
     def submit(self, model: type[Model]):
         assert issubclass(model, Model)
@@ -56,9 +66,9 @@ class OracleHub:
             db = pickle.load(f)
         for model_hash in db:
             obj = db[model_hash]
-            glbs, locs = {}, {}
-            exec(obj["source"], glbs, locs)
-            obj["model"] = locs[obj["name"]]
+            namespace = {}
+            exec(obj["source"], namespace)
+            obj["model"] = namespace[obj["name"]]
         self.db = db
 
     def write(self):
@@ -73,7 +83,7 @@ class OracleHub:
     def dump(self, path: Path = None, model_hash=None):
         path = path or self.db_file.parent
         if model_hash:
-            models = {model_hash: self[model_hash]}
+            models = {model_hash: self.db[model_hash]}
         else:
             models = self.db
         for model_hash in models:
@@ -81,16 +91,33 @@ class OracleHub:
             with open(py_file, "w") as f:
                 f.write(models[model_hash]["source"])
 
-    def run(self, model_hash: str, data: pd.DataFrame):
-        nuts = NUTS(self[model_hash])
+    def run(self, model: Model | str, data, sample_params=None):
+        sample_params = sample_params or {}
+        if isinstance(model, str):
+            model = self[model]
+        return self.sample(model.predict, data, **sample_params)
+
+    def sample(
+        self,
+        model_fn: function,
+        data,
+        nuts_params=None,
+        mcmc_params=None,
+    ):
+        nuts_params = nuts_params or {}
+        mcmc_params = mcmc_params or {}
+
+        nuts = NUTS(model_fn, **nuts_params)
+        mcmc = MCMC(nuts, num_samples=1000, num_warmup=1000, **mcmc_params)
+        mcmc.run(PRNGKey(0), data=data)
+        return mcmc
 
     def compare(
         self,
         model_hashes: list[str],
         data,
         alpha=1.0,
-        nuts_params=None,
-        mcmc_params=None,
+        sample_params=None,
     ):
         N_models = len(model_hashes)
         models = {model_hash: self[model_hash] for model_hash in model_hashes}
@@ -119,13 +146,10 @@ class OracleHub:
             for var_name, mix_pred in mixtures.items():
                 sample(var_name, mix_pred, obs=data[var_name])
 
-        nuts = NUTS(numpyro_model)
-        mcmc = MCMC(nuts, num_samples=1000, num_warmup=1000, num_chains=1)
-        mcmc.run(PRNGKey(0), data=data)
-        return mcmc
+        sample_params = sample_params or {}
+        self.sample(numpyro_model, data, **sample_params)
 
     def __getitem__(self, model_hash) -> Model:
-        return self.db[model_hash]
-
-
-# %%
+        if self._meta is None:
+            raise Exception("Metadata not loaded")
+        return self.db[model_hash]["model"](self._meta)
